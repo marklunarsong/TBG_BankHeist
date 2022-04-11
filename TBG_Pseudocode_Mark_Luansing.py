e501b9bde42ee7de9008fb3627b4643598a5d430
marklunarsong <101216276+marklunarsong@users.noreply.github.com>
2022-04-11 14:59:32 -0600

    Add files via upload

---
+++ TBG_Pseudocode_Mark_Luansing.py
@@ -0,0 +1,46 @@
+# TBG Pseudocode - Mark Luansing
+# April 8th, 2022
+
+# This python file contains short pseudocode of my
+# TBG, for the start of what the game will look like
+
+Print exposition diaglogue (Setting, plot, you as a character, and what your goal is)
+Print naration (The player enters the warehouse and meets the crew)
+
+Create a list of gadgets for the heist
+
+Print dialogue (The crew asks the player what gadgets they want to bring to the mission)
+Create loop for the gadgets in gadgets_list
+    print gadgets_list
+
+Player types input to select gadget 1
+Sets chosen gadget boolean to true
+Add gadget to gadgets_inventory list
+
+Reprint dialogue to ask the player to pick another gadget
+Player types input to select gadget 2
+Sets chosen gadget boolean to true
+Add gadget to gadgets_inventory list
+
+Create a list of escape_vehicles
+
+Print dialogue (The crew asks the player what vehicle they want to use for the mission)
+Create loop for the vehicles in escape vehicles list
+    print escape_vehicles list
+Add vehicle selected boolean to true
+
+Create a list of entrance_choices
+
+Print dialogue (The crew asks where you want to enter the bank from)
+Create loop for the entrances in entrance_choices list
+    print entrance_choices list
+Add entrance selected boolean to true
+
+Print dialogue (the night of the heist arrives and you enter the bank from where you chose)
+
+
+
+
+
+
+
